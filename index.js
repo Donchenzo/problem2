18ee815c6b3eb8c9530fe097dc6b357ed8621667
Donchenzo <ezikadereckmain@outlook.com>
2023-05-16 04:56:39 -0500

    new

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1).
+
+// Create a JavaScript code that accepts a string of lowercase letters. Print the word followed by how many consonants and vowels it has.
+
+// "a", "e", "i", "o", and "u" are considered vowels. 
+
+// Every other letter is considered a consonant.
+
+// Hint: Define two variables that keep track of the number of consonants and vowels.
+
+// 2).Create a JavaScript code that accepts an array of numbers and returns a new array with only the odd numbers from the original array. If there is only one odd number in the array, return an array with that single value.
+
+
+function some(){
+    let i = 0; i
+}
+
+
+
+// function some(reversed, og){
+// for (let i = 0; i >= og.length - 1;  i++) {
+//     console.log (([reversed.push(og[i])]) , [og]);
+//   }
+// }
+//   some([],[1, 2, 3, 4, 5])
